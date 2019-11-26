d417eac51d9a024d2c82684c8165b6c606c47f01
Haris Sucipto <harisssucipto@gmail.com>
2019-11-26 11:15:02 +0700

    add sanpshot testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 // .storybook/config.js
 
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
 import '../src/index.css';
 
-const req = require.context('../src', true, /\.stories.js$/);
+const req = requireContext('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
